e5cb362370e90b3f734f5d774311a71fe81e2339
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2007-06-04 01:06:40 +0000

    - some crap

--- trunk/applications/lazysat/alchemy-learn-weights.sh
+++ trunk/applications/lazysat/alchemy-learn-weights.sh
@@ -1,6 +1,4 @@
-~/opt/alchemy/bin/learnwts -i cora-all.mln -o cora-all.trained.mln -t fold0of10.fold -noAddUnitClauses \
-  -d true -ne SameBib,SameAuthor,SameTitle,SameVenue -infer "-m -lazy"
-
-  ~/opt/alchemy/bin/learnwts -noAddUnitClauses true -i cora-all.mln -o cora-all.trained.mln \
-   -t fold1of10.fold,fold2of10.fold,fold3of10.fold,fold4of10.fold,fold5of10.fold,fold6of10.fold,fold7of10.fold,fold8of10.fold,fold9of10.fold -d true -ne SameBib,SameAuthor,SameTitle,SameVenue -infer "-m -lazy" -multipleDatabases true 
+~/opt/alchemy/bin/learnwts -noAddUnitClauses true -i cora-all.mln -o cora-all.trained-for-fold0.mln \
+   -t 1of10.fold.db,2of10.fold.db,3of10.fold.db,4of10.fold.db,5of10.fold.db,6of10.fold.db,7of10.fold.db,8of10.fold.db,9of10.fold.db \
+   -d true -ne SameBib,SameAuthor,SameTitle,SameVenue -infer "-m -lazy" -multipleDatabases true
 
