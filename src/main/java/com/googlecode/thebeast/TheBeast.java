7220d3bb8e3c57063f54c62f22dfb7cab1529475
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2008-06-15 15:13:15 +0000

    Now the maven project creates a sensible jar file

--- src/main/java/com/googlecode/thebeast/TheBeast.java
+++ src/main/java/com/googlecode/thebeast/TheBeast.java
@@ -5,14 +5,26 @@ package com.googlecode.thebeast;
  *
  * @author Sebastian Riedel
  */
-public class TheBeast {
+public final class TheBeast {
+
+  /**
+   * Hide the default constructor.
+   */
+  private TheBeast() {
+
+  }
+
+  /**
+   * Version string. Should be updated when a new version is to be released.
+   */
+  public static final String VERSION = "1.0.0-SNAPSHOT";
 
   /**
    * Does not do much yet.
    *
    * @param args array of parameters, not used yet.
    */
-  public static void main(String[] args) {
-
+  public static void main(final String[] args) {
+    System.out.println("markov thebeast " + VERSION);
   }
 }
