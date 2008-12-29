436466e04ffda25651ea2c0f688c6ff72273a9fd
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2008-12-29 15:51:08 +0000

    - test refactoring

--- branches/thefuture/src/test/java/com/googlecode/thebeast/pml/TestGroundMarkovNetworkGroundingSimpleClause.java
+++ branches/thefuture/src/test/java/com/googlecode/thebeast/pml/TestGroundMarkovNetworkGroundingSimpleClause.java
@@ -5,6 +5,7 @@ import com.googlecode.thebeast.query.NestedSubstitution;
 import com.googlecode.thebeast.world.SocialNetworkFixture;
 import com.googlecode.thebeast.world.sql.SQLSignature;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import static org.testng.Assert.assertEquals;
 
 import java.util.List;
@@ -12,34 +13,92 @@ import java.util.List;
 /**
  * @author Sebastian Riedel
  */
-public class TestPseudoGroundMarkovNetwork {
+public class TestGroundMarkovNetworkGroundingSimpleClause {
+  private GroundMarkovNetwork gmn;
+  private SocialNetworkFixture fixture;
+  private PMLClause clause;
+  private List<NestedSubstitution> substitutions;
 
-  @Test
-  public void testGround() {
-    GroundMarkovNetwork gmn = new GroundMarkovNetwork();
-    SocialNetworkFixture fixture =
-      new SocialNetworkFixture(SQLSignature.createSignature());
 
+  @BeforeMethod
+  public void setUp() {
+
+    gmn = new GroundMarkovNetwork();
+    fixture = new SocialNetworkFixture(SQLSignature.createSignature());
     ClauseBuilder builder = new ClauseBuilder(
       QueryFactory.getInstance(), fixture.signature);
-
-    PMLClause clause = builder.
+    clause = builder.
       atom(fixture.friends, "x", "y").
       atom(fixture.signature.getIntegerType().getEquals(), "i", "0").
       atom(fixture.signature.getDoubleType().getEquals(), "s", "1.0").
       body().
       head(fixture.friends, "y", "x").
       clause(Exists.EXISTS, "i", "s");
+    substitutions = NestedSubstitution.createNestedSubstitutions(fixture.signature,
+      "x/Peter y/Anna i/0 s/1.0",
+      "x/Peter y/Anna i/1 s/1.0");
+  }
 
+  @Test
+  public void testGroundCreatesFactorWithOriginalClause() {
     List<GroundFactor> factors = gmn.ground(clause,
-      NestedSubstitution.createNestedSubstitutions(fixture.signature,
-        "x/Peter y/Anna i/0 s/1.0",
-        "x/Peter y/Anna i/1 s/1.0"));
+      substitutions);
 
+    GroundFactor factor = factors.get(0);
+    assertEquals(clause, factor.getClause());
+  }
+
+  @Test
+  public void testGroundCreatesBodyWithRightNumberOfAtoms() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
+
+    GroundFactor factor = factors.get(0);
+    assertEquals(3, factor.getBody().size());
+  }
+
+  @Test
+  public void testGroundCreatesBodyAtomWithRightPredicate() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
+
+    GroundFactor factor = factors.get(0);
+    assertEquals(fixture.friends, factor.getBody().get(0).getPredicate());
+  }
+
+  @Test
+  public void testGroundCreatesBodyWithCorrectAtomArgument() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
+
+    GroundFactor factor = factors.get(0);
+    assertEquals(fixture.anna, factor.getBody().get(0).getArguments().get(1));
+  }
+
+  @Test
+  public void testGroundCreatesBodyWithRightStaticPredicate() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
+
+    GroundFactor factor = factors.get(0);
+    assertEquals(fixture.signature.getIntegerType().getEquals(),
+      factor.getBody().get(1).getPredicate());
+  }
+
+  @Test
+  public void testGroundCreatesRightNumberOfFactors() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
     assertEquals(2, factors.size());
+  }
+
+  @Test
+  public void testGround() {
+    List<GroundFactor> factors = gmn.ground(clause,
+      substitutions);
+
     GroundFactor factor = factors.get(0);
 
-    assertEquals(clause, factor.getClause());
     assertEquals(3, factor.getBody().size());
     assertEquals(fixture.friends, factor.getBody().get(0).getPredicate());
     assertEquals(fixture.peter, factor.getBody().get(0).getArguments().get(0));
@@ -73,4 +132,6 @@ public class TestPseudoGroundMarkovNetwork {
 
     // Add your code here
   }
+
+
 }
