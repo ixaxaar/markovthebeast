d56d9330b409c156ce64a4dd0a345efca8d959c3
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-02-06 01:20:49 +0000

    - refactorings

--- src/main/scala/org/riedelcastro/thebeast/Predicate.scala
+++ src/main/scala/org/riedelcastro/thebeast/Predicate.scala
@@ -7,7 +7,7 @@ import org.scalatest.Suite
  * @author Sebastian Riedel
  */
 
-class FunctionSymbol[+T, +R](name: String, domain: Seq[T], range: Seq[R])
+class FunctionSymbol[+T, +R](val name: String, val domain: Seq[T], val range: Seq[R])
 
 object AllFunctions {
   def apply[T, R](domain: Iterable[T], range: Iterable[R]): Stream[Map[T, R]] = {
@@ -16,23 +16,26 @@ object AllFunctions {
   }
 }
 
-trait Signature extends Seq[FunctionSymbol[Any, Any]]
+trait Signature extends Seq[FunctionSymbol[Any, Nothing]]
 
-class PossibleWorld extends HashMap[FunctionSymbol[Any, Any], Any => Any] {
-  def getFunction(symbol: FunctionSymbol[Any, Any]): Any => Any = apply(symbol)
+class PossibleWorld {
+  val functions = new HashMap[FunctionSymbol[Any, Any], Any => Any]
 
-  def setFunction(symbol: FunctionSymbol[Any, Any], function: Any => Any) =
-    +=((symbol, function))
+  def getFunction[T, R](symbol: FunctionSymbol[T, R]): T => R =
+    functions(symbol.asInstanceOf[FunctionSymbol[Any, Any]]).asInstanceOf[T => R]
+
+  def setFunction[T, R](symbol: FunctionSymbol[T, R], function: T => R) =
+    functions += (symbol.asInstanceOf[FunctionSymbol[Any, Any]] -> function.asInstanceOf[Any => Any])
 
 }
 
-abstract class Formula {
-  def ground(substitution: Binding): Scorer
+abstract class Formula[S <: Score] {
+  def ground(substitution: Binding): Scorer[S]
 }
 
-case class Atom[T, R](symbol: FunctionSymbol[T, R], from: Term[T], to: Term[R]) extends Formula {
-  def ground(binding: Binding): Scorer = {
-    GroundAtom[T, R](symbol, from.resolve(binding), to.resolve(binding))
+case class Atom[T, R](symbol: FunctionSymbol[T, R], from: Term[T], to: Term[R]) extends Formula[BoolScore] {
+  def ground(binding: Binding): Scorer[BoolScore] = {
+    GroundAtomScorer[T, R](GroundAtom(symbol, from.resolve(binding), to.resolve(binding)))
   }
 }
 
@@ -65,51 +68,58 @@ case class FunApp[T, R](val f: T => R, val arg: Term[T]) extends Term[R] {
 }
 
 trait Score {
-  def value() : Double;
+  def value(): Double;
 }
 
-case class DoubleScore(val score:Double) extends Score {
+case class DoubleScore(val score: Double) extends Score {
   def value = score
 }
 
-case class BoolOneScore(val score:Boolean) extends Score {
+case class BoolScore(val score: Boolean) extends Score {
   def value = if (score) 1.0 else 0.0
 }
-case class IntScore(val score:Int) extends Score {
+case class IntScore(val score: Int) extends Score {
   def value = score
 }
 
-trait Scorer {
-  def score(world: PossibleWorld): Double
+trait Scorer[S <: Score] {
+  def score(world: PossibleWorld): S
 }
 
-case class GroundAtom[T, R](val symbol: FunctionSymbol[T, R], val from: T, to: R) extends Scorer {
-  def score(world: PossibleWorld): Double = if (world.getFunction(symbol)(from) == to) 1.0 else 0.0
+case class GroundAtom[T, R](val symbol: FunctionSymbol[T, R], val from: T, val to: R);
+
+case class GroundAtomScorer[T, R](val atom: GroundAtom[T, R]) extends Scorer[BoolScore] {
+  def score(world: PossibleWorld): BoolScore = BoolScore(world.getFunction(atom.symbol)(atom.from) == atom.to)
 }
 
-case class BinaryCombinator[L <: Scorer, R <: Scorer](val function: (Double, Double) => Double,
-                                                     val left: L,
-                                                     val right: R) extends Scorer {
-  def score(world: PossibleWorld) = function(left.score(world), right.score(world))
+object And extends ((Score, Score) => BoolScore) {
+  def apply(arg1: Score, arg2: Score): BoolScore = BoolScore(arg1.value > 0 && arg2.value > 0)
 }
 
+object Count extends ((Score, Score) => IntScore) {
+  def sign(value: Double): Int = if (value > 0.0) 1 else 0
 
-case class Min[L <: Scorer, R <: Scorer](val left: L, val right: R) extends Scorer {
-  def score(world: PossibleWorld): Double = Math.min(left.score(world), right.score(world))
+  def apply(arg1: Score, arg2: Score): IntScore = IntScore(sign(arg1.value) + sign(arg2.value))
 }
 
-case class FoldLeft(val f: (Double, Double) => Double, scorers: Seq[Scorer]) extends Scorer {
-  def score(world: PossibleWorld) = scorers.foldLeft(0.0){(result, scorer) => f(result, scorer.score(world))}
+case class Apply[A1 <: Score, A2 <: Score, R <: Score](val function: (A1, A2) => R,
+                                                      val arg1: Scorer[A1],
+                                                      val arg2: Scorer[A2]) extends Scorer[R]{
+  def score(world: PossibleWorld) = function(arg1.score(world),arg2.score(world))
 }
 
-case class FormulaScorer(val formula: Formula, val binding: Binding) extends Scorer {
-  def score(world: PossibleWorld) = formula.ground(binding).score(world)
+case class Fold[T <: Score, R <: Score](val f: (T, R) => T, scorers: Seq[Scorer[R]], val init: T) extends Scorer[T] {
+  def score(world: PossibleWorld): T = scorers.foldLeft(init){(result, scorer) => f(result, scorer.score(world))}
+}
+
+case class FormulaScorer[S <: Score](val formula: Formula[S], val binding: Binding) extends Scorer[S] {
+  def score(world: PossibleWorld): S = formula.ground(binding).score(world)
 }
 
 object Example {
   def main(args: Array[String]) {
     val set = new FunctionSymbol("set", Array(1, 2, 3), Array(true, false))
-    val scorer = new Min(GroundAtom(set, 1, true), GroundAtom(set, 2, false))
+    val scorer = new Apply(And,GroundAtomScorer(GroundAtom(set, 1, true)), GroundAtomScorer(GroundAtom(set, 2, false)))
     val world = new PossibleWorld()
     world.setFunction(set, Set(1))
     println(scorer.score(world))
