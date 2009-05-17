8eb6c78fbf0398cbc66291a4fd71b153e9a8c649
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-05-17 18:01:00 +0000

    - implemented an exhaustive argmax solver

--- branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Env.scala
+++ branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Env.scala
@@ -1,4 +1,5 @@
 package org.riedelcastro.thebeast.env
+
 /**
  * @author Sebastian Riedel
  */
@@ -15,18 +16,20 @@ trait Env {
           val arg = eval(argTerm);
           if (fun.isDefined && arg.isDefined) Some(fun.get(arg.get)) else None
         }
-      case Fold(funTerm,argTerms,initTerm) =>
+      case Fold(funTerm, argTerms, initTerm) =>
         if (argTerms.isEmpty)
           eval(initTerm)
-        else 
-          eval(FunApp(FunApp(funTerm, Fold(funTerm,argTerms.drop(1),initTerm)),argTerms(0)))
+        else
+          eval(FunApp(FunApp(funTerm, Fold(funTerm, argTerms.drop(1), initTerm)), argTerms(0)))
+      case x => eval(ground(x))
     }
   }
 
   def ground[T](term: Term[T]): Term[T] = {
     term match {
+      case q: Quantification[_, _, _] => ground(q.grounded)
       case FunApp(f, arg) => FunApp(ground(f), ground(arg))
-      case Fold(f,args,init) => Fold(ground(f), args.map(a => ground(a)), ground(init))
+      case Fold(f, args, init) => Fold(ground(f), args.map(a => ground(a)), ground(init))
       case c: Constant[_] => c
       case v: Var[_] => {val x = eval(v); if (x.isDefined) Constant(x.get) else v}
     }
@@ -38,16 +41,36 @@ trait Env {
 }
 
 class MutableEnv extends Env {
-  private[this] type MutableMap = scala.collection.mutable.HashMap[Any, Any]
-  private[this] val values = new MutableMap
+  private type MutableMapCheck = scala.collection.mutable.HashMap[_, _]
+  private type MutableMap = scala.collection.mutable.HashMap[Any, Any]
+  private var values = new MutableMap
 
   def resolveVar[T](variable: Var[T]) = values.get(variable).asInstanceOf[Option[T]]
 
-  private[this] def getMap(variable: EnvVar[Any]): MutableMap = {
+  private def getMap(variable: EnvVar[Any]): MutableMap = {
     variable match {
       case v: Var[_] => values.getOrElseUpdate(v, new MutableMap()).asInstanceOf[MutableMap]
       case FunAppVar(funVar, arg) => getMap(funVar).getOrElseUpdate(arg, new MutableMap()).asInstanceOf[MutableMap]
     }
+  }
+
+
+  override def clone = {
+    val result = new MutableEnv
+    result.values = cloneMutableMap(values)
+    result
+  }
+
+  private def cloneMutableMap(map: MutableMap): MutableMap = {
+    val result = new MutableMap
+    map foreach {
+      case (key, value) =>
+        if (value.isInstanceOf[MutableMapCheck])
+          result += (key ->cloneMutableMap(value.asInstanceOf[MutableMap]))
+        else
+          result += (key -> value)
+    }
+    result
   }
 
   def set[T](variable: EnvVar[T], value: T) {
