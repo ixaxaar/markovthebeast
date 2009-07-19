a74bd2b8e8b0ab95a2e98f941fc3fd9ceb4ac887
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-07-19 14:43:35 +0000

    - Improved closed world handling of functions---previously if f(*)(*) was closed and f(A)(1) set then f(A)(2) would still be unset rather than the default value.

--- branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Env.scala
+++ branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Env.scala
@@ -1,7 +1,7 @@
 package org.riedelcastro.thebeast.env
 
 
-import collection.mutable.HashSet
+import collection.mutable.{MapProxy, HashSet}
 
 /**
  * @author Sebastian Riedel
@@ -27,10 +27,29 @@ class MaskedEnv(var unmasked: Env, var hiddenVariables: Set[EnvVar[_]]) extends 
   }
 }
 
+private class MutableMap extends scala.collection.mutable.HashMap[Any, Any] {
+  private class ClosedMutableMap(var self: MutableMap, signature: FunctionValues[_, _])
+          extends MutableMap with MapProxy[Any, Any] {
+
+    override def default(a: Any) = signature.range.defaultValue
+
+    override def apply(a: Any) = self.get(a) match {
+      case Some(x: MutableMap) => x.close(signature.range.asInstanceOf[FunctionValues[_, _]])
+      case Some(_) => super.apply(a)
+      case None => default(a)
+    }
+    
+  }
+
+  def close(signature: FunctionValues[_, _]): MutableMap = {
+    new ClosedMutableMap(this, signature)
+  }
+
+}
 
 class MutableEnv extends Env {
   private type MutableMapCheck = scala.collection.mutable.HashMap[_, _]
-  private type MutableMap = scala.collection.mutable.HashMap[Any, Any]
+
   private var values = new MutableMap
   private var closed = new HashSet[EnvVar[_]]
 
@@ -39,11 +58,15 @@ class MutableEnv extends Env {
       case v: Var[_] => values.get(variable).asInstanceOf[Option[T]]
       case FunAppVar(funVar, arg) => getMap(funVar).get(arg).asInstanceOf[Option[T]]
     }
-    result match {
-      case Some(x) => result
-      case None => if (closed.contains(variable)) Some(variable.values.defaultValue) else None
-    }
+    if (closed.contains(variable)) convertToClosed(variable, result) else result
+  }
 
+  def convertToClosed[T](variable: EnvVar[T], value: Option[T]): Option[T] = {
+    value match {
+      case Some(x: MutableMap) => Some(x.close(variable.values.asInstanceOf[FunctionValues[_, _]]).asInstanceOf[T])
+      case Some(_) => value
+      case None => Some(variable.values.defaultValue)
+    }
   }
 
   private def getMap(variable: EnvVar[Any]): MutableMap = {
@@ -88,7 +111,7 @@ class MutableEnv extends Env {
   def +=[T](mapping: Tuple2[EnvVar[T], T]) = set(mapping._1, mapping._2)
 
 
-  def mapTo[T](envVar: EnvVar[T]) = new VarWithEnv(envVar,this)
+  def mapTo[T](envVar: EnvVar[T]) = new VarWithEnv(envVar, this)
 
 
 }
--- branches/scalabeast/src/test/scala/org/riedelcastro/thebeast/env/MutableEnvSpecification.scala
+++ branches/scalabeast/src/test/scala/org/riedelcastro/thebeast/env/MutableEnvSpecification.scala
@@ -17,20 +17,26 @@ class MutableEnvSpecification extends Specification with TheBeastEnv with JUnit 
       env(x) must_== 1
     }
     "set partial function mappings" in {
-      var f = "f" <~ Values(1,2,3) -> (Values("A","B") -> Values(true,false))
+      var f = "f" <~ Values(1,2,3) -> (Values("A","B") -> Values(false,true))
       var env = new MutableEnv
       env.mapTo(f)(1)("A") ->true
       env(f(1)("A")) must_== true
     }
+    "return a default value for unset closed variables" in {
+      var f = "f" <~ Values(1,2,3) -> (Values("A","B") -> Values(false,true))
+      var env = new MutableEnv
+      env.close(f,true)
+      env.mapTo(f)(1)("A") ->true
+      env(f(1)("B")) must_== false
+    }
     "mask an already set variable" in {
-      var f = "f" <~ Values(1,2,3) -> (Values("A","B") -> Values(true,false))
+      var f = "f" <~ Values(1,2,3) -> (Values("A","B") -> Values(false,true))
       var env = new MutableEnv
       env.mapTo(f)(1)("A") ->true
       env.mask(Set(f)).eval(f(1)("A")) must_== None
     }
-
     "return None for unset variables" in {
-      var f = "f" <~ Values(1,2,3) -> Values(true,false)
+      var f = "f" <~ Values(1,2,3) -> Values(false,true)
       var env = new MutableEnv
       env.eval(f(1)) must_== None      
     }
