109e0316fb652200a106130b3dc79a764adeff0e
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-08-03 01:25:34 +0000

    started to implement tuple support

--- branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Values.scala
+++ branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Values.scala
@@ -13,6 +13,8 @@ trait Values[+T] extends Iterable[T] {
   def randomValue: T = {val seq = toSeq; seq(new scala.util.Random().nextInt(seq.size))}
 
   def createVariable(name: String): Var[T] = new Var(name, this)
+
+
 }
 
 object Values {
@@ -21,6 +23,51 @@ object Values {
 }
 
 class ValuesProxy[T](override val self: Iterable[T]) extends Values[T] with IterableProxy[T]
+
+case class TupleValues2[T1, T2](_1: Values[T1], _2: Values[T2]) extends Values[Tuple2[T1, T2]] {
+  def elements = Util.Cartesian.cartesianProduct(Seq(_1.toStream, _2.toStream)).map(
+    seq => Tuple2(seq(0).asInstanceOf[T1], seq(1).asInstanceOf[T2])).elements
+
+  def x[T3](_3:Values[T3]) = TupleValues3(_1,_2,_3)
+
+}
+case class TupleValues3[T1, T2, T3](_1: Values[T1], _2: Values[T2], _3: Values[T3]) extends Values[Tuple3[T1, T2, T3]] {
+  def elements = Util.Cartesian.cartesianProduct(Seq(_1.toStream, _2.toStream, _3.toStream)).map(
+    seq => Tuple3(
+      seq(0).asInstanceOf[T1],
+      seq(1).asInstanceOf[T2],
+      seq(2).asInstanceOf[T3]))
+          .elements
+  def x[T4](_4:Values[T4]) = TupleValues4(_1,_2,_3,_4)
+
+}
+
+case class TupleValues4[T1, T2, T3, T4](_1: Values[T1], _2: Values[T2], _3: Values[T3], _4: Values[T4])
+        extends Values[Tuple4[T1, T2, T3, T4]] {
+  def elements = Util.Cartesian.cartesianProduct(Seq(_1.toStream, _2.toStream, _3.toStream, _4.toStream)).map(
+    seq => Tuple4(
+      seq(0).asInstanceOf[T1],
+      seq(1).asInstanceOf[T2],
+      seq(2).asInstanceOf[T3],
+      seq(3).asInstanceOf[T4]))
+          .elements
+
+  def x[T5](_5:Values[T5]) = TupleValues5(_1,_2,_3,_4,_5)
+
+}
+
+case class TupleValues5[T1, T2, T3, T4, T5](_1: Values[T1], _2: Values[T2], _3: Values[T3], _4: Values[T4], _5: Values[T5])
+        extends Values[Tuple5[T1, T2, T3, T4, T5]] {
+  def elements = Util.Cartesian.cartesianProduct(Seq(_1.toStream, _2.toStream, _3.toStream, _4.toStream, _5.toStream)).map(
+    seq => Tuple5(
+      seq(0).asInstanceOf[T1],
+      seq(1).asInstanceOf[T2],
+      seq(2).asInstanceOf[T3],
+      seq(3).asInstanceOf[T4],
+      seq(4).asInstanceOf[T5]))
+          .elements
+}
+
 
 case class FunctionValues[T, R](val domain: Values[T], val range: Values[R]) extends Values[FunctionValue[T, R]] {
   def elements = Util.AllFunctions(domain.toStream, range.toStream).map(m => toFunctionValue(m)).elements
