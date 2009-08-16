80b8fdf59e7dfa28267fff3017feed63bb9f6d69
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-08-16 01:49:31 +0000

    - began marginal inference (exhaustive)

--- branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Beliefs.scala
+++ branches/scalabeast/src/main/scala/org/riedelcastro/thebeast/env/Beliefs.scala
@@ -8,17 +8,23 @@ import collection.mutable.HashMap
  */
 
 trait Beliefs {
-  def belief[T](term:Term[T]) : Belief[T]  
+  def belief[T](term:Term[T]) : Belief[T]
 }
 
-class MutableBeliefs extends Beliefs {
-  private val beliefs = new HashMap[Term[Any],Any]
+class MutableBeliefs extends Beliefs  {
+  private val beliefs = new HashMap[Term[Any],Belief[Any]]
   
   def setBelief[T](term:Term[T],belief:Belief[T]) = {
-    beliefs(term) = belief
+    beliefs(term.asInstanceOf[Term[Any]]) = belief.asInstanceOf[Belief[Any]]
   }
 
-  def belief[T](term: Term[T]) = beliefs(term).asInstanceOf[Belief[T]]
+  def increaseBelief[T](term:Term[T], value:T, delta:Double) = {
+    var belief = beliefs.getOrElseUpdate(term,
+      new MutableBelief[T](term.values).asInstanceOf[MutableBelief[Any]]).asInstanceOf[MutableBelief[T]]
+    belief.increaseBelief(value,delta)
+  }
+
+  def belief[T](term: Term[T]) : Belief[T] = beliefs(term).asInstanceOf[Belief[T]]
 }
 
 sealed trait Belief[T] {
@@ -26,3 +32,13 @@ sealed trait Belief[T] {
   def belief(value:T) : Double
 }
 
+class MutableBelief[T](val values:Values[T]) extends Belief[T] {
+  private val _belief = new HashMap[T,Double]
+
+  def belief(value: T) = _belief(value)
+
+  def setBelief(value:T, belief:Double) = _belief(value) = belief
+
+  def increaseBelief(value:T, belief:Double) = _belief(value) = _belief.getOrElse(value, 0.0) + belief
+
+}
