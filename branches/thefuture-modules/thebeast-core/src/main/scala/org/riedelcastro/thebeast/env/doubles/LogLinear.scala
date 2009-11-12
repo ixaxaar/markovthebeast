3e90ab7f27ff96d8f369fb665f3dd198c2272b6a
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2009-11-12 05:47:52 +0000

    - added default implementation for loglinear marginalize (that includes marginals for suff statistics)

--- branches/thefuture-modules/thebeast-core/src/main/scala/org/riedelcastro/thebeast/env/doubles/LogLinear.scala
+++ branches/thefuture-modules/thebeast-core/src/main/scala/org/riedelcastro/thebeast/env/doubles/LogLinear.scala
@@ -2,26 +2,26 @@ package org.riedelcastro.thebeast.env.doubles
 
 import collection.mutable.HashMap
 import org.riedelcastro.thebeast.env.vectors.{Vector, VectorDotApp, VectorVar, VectorTerm}
-import org.riedelcastro.thebeast.env.{EnvVar, Beliefs}
+import org.riedelcastro.thebeast.env.{Term, MutableEnv, EnvVar, Beliefs}
+import org.riedelcastro.thebeast.solve.ExhaustiveMarginalInference
 
 /**
  */
 case class LogLinear(sufficient:VectorTerm, weights:VectorVar, bias:DoubleTerm)
   extends Exp(Sum(Seq(VectorDotApp(sufficient,weights),bias))) {
 
-  def marginalizeLogLinear(incoming:Beliefs[Any,EnvVar[Any]], weights:Weights) : LogLinearBeliefs = {
+  def marginalizeLogLinear(incoming:Beliefs[Any,EnvVar[Any]], weights:Weights) : Beliefs[Any,Term[Any]] = {
     //default implementation
-    //ground weight variables (throw error if not all weights are defined)
-    //create auxiliary problem: $(s == suff) * exp(s dot weight)
-    //problem: s would be real valued (multidimensional), so we can do exhaustive inference on it
-    //so instead: write additional exhaustive method that evaluates one extra term in addition to the
-    //score (i.e. get expectation? no) 
-
-    null
+    val env = new MutableEnv
+    //set weight variables in environment
+    for (variable <- weights.keySet) env(variable) = weights(variable)
+    //create the grounded term (that doesn't have weight variables)
+    val grounded = ground(env)
+    //the queries are the variables plus the sufficient statistics
+    val queries = variables ++ Set(sufficient)
+    ExhaustiveMarginalInference.marginalizeQueries(grounded,incoming,queries)
   }
 
 }
 
 class Weights extends HashMap[VectorVar,Vector]
-
-class LogLinearBeliefs
--- branches/thefuture-modules/thebeast-core/src/main/scala/org/riedelcastro/thebeast/solve/MarginalInference.scala
+++ branches/thefuture-modules/thebeast-core/src/main/scala/org/riedelcastro/thebeast/solve/MarginalInference.scala
@@ -4,56 +4,64 @@ import org.riedelcastro.thebeast._
 import env._
 import env.doubles._
 import util.{Trackable}
+
 /**
  * @author Sebastian Riedel
  */
 
 trait MarginalInference {
-  def infer(term: DoubleTerm): Beliefs[Any,EnvVar[Any]]
+  def infer(term: DoubleTerm): Beliefs[Any, EnvVar[Any]]
 
 }
 
 object ExhaustiveMarginalInference extends MarginalInference with Trackable {
 
-  def marginalize(term:DoubleTerm, incoming: Beliefs[Any,EnvVar[Any]]): Beliefs[Any,EnvVar[Any]] = {
+  //todo: a lot of code duplication here, get rid of this
+  def marginalize(term: DoubleTerm, incoming: Beliefs[Any, EnvVar[Any]]): Beliefs[Any, EnvVar[Any]] = {
     val multiplied = term * Multiplication(term.variables.map(v => BeliefTerm(incoming.belief(v), v)).toSeq)
     ExhaustiveMarginalInference.infer(multiplied)
   }
 
+  def marginalizeQueries(term: DoubleTerm,
+                         incoming: Beliefs[Any, EnvVar[Any]],
+                         queries: Collection[Term[Any]]): Beliefs[Any, Term[Any]] = {
+    val multiplied = term * Multiplication(term.variables.map(v => BeliefTerm(incoming.belief(v), v)).toSeq)
+    ExhaustiveMarginalInference.inferQueries(multiplied, queries)
+  }
 
-  def infer(term: DoubleTerm) : Beliefs[Any,EnvVar[Any]] = term match {
+  def infer(term: DoubleTerm): Beliefs[Any, EnvVar[Any]] = term match {
     case Normalize(x) => inferExhaustively(x).normalize
     case _ => inferExhaustively(term)
   }
 
-  def inferQueries(term: DoubleTerm, queries:Seq[Term[Any]]) : Beliefs[Any,Term[Any]] = term match {
-    case Normalize(x) => inferQueriesExhaustively(x,queries).normalize
-    case _ => inferQueriesExhaustively(term,queries)
+  def inferQueries(term: DoubleTerm, queries: Collection[Term[Any]]): Beliefs[Any, Term[Any]] = term match {
+    case Normalize(x) => inferQueriesExhaustively(x, queries).normalize
+    case _ => inferQueriesExhaustively(term, queries)
   }
 
 
-  private def inferExhaustively(term: DoubleTerm) : Beliefs[Any,EnvVar[Any]] = {
+  private def inferExhaustively(term: DoubleTerm): Beliefs[Any, EnvVar[Any]] = {
     val domain = term.variables.toSeq
     gatherBeliefs(term, domain, domain)
   }
 
-  private def inferQueriesExhaustively(term: DoubleTerm, queries:Seq[Term[Any]]) : Beliefs[Any,Term[Any]] = {
+  private def inferQueriesExhaustively(term: DoubleTerm, queries: Collection[Term[Any]]): Beliefs[Any, Term[Any]] = {
     val domain = term.variables.toSeq
     gatherBeliefs(term, domain, queries)
   }
 
-  private def gatherBeliefs[V,T<:Term[V]](term:DoubleTerm,
-                                            variables:Collection[EnvVar[Any]], 
-                                            queries:Seq[T]) : Beliefs[V,T] = {
+  private def gatherBeliefs[V, T <: Term[V]](term: DoubleTerm,
+                                             variables: Collection[EnvVar[Any]],
+                                             queries: Collection[T]): Beliefs[V, T] = {
     |**("Exhaustive marginal inference for " + term)
 
-    val beliefs = new MutableBeliefs[V,T]
+    val beliefs = new MutableBeliefs[V, T]
 
     Env.forall(variables) {
       env => {
         val score = env(term);
         for (query <- queries) {
-          beliefs.increaseBelief(query,env(query), score)
+          beliefs.increaseBelief(query, env(query), score)
         }
       }
     }
@@ -67,11 +75,12 @@ object ExhaustiveMarginalInference extends MarginalInference with Trackable {
 }
 
 object OptimizedMarginalizer {
-  def marginalize(term:DoubleTerm, incoming: Beliefs[Any,EnvVar[Any]]): Beliefs[Any,EnvVar[Any]] = term match {
-    case Exp(Multiplication(Seq(Indicator(x),y:DoubleTerm ))) => null
-    case x => defaultMarginalize(x,incoming) 
+  def marginalize(term: DoubleTerm, incoming: Beliefs[Any, EnvVar[Any]]): Beliefs[Any, EnvVar[Any]] = term match {
+    case Exp(Multiplication(Seq(Indicator(x), y: DoubleTerm))) => null
+    case x => defaultMarginalize(x, incoming)
   }
-  def defaultMarginalize(term:DoubleTerm, incoming: Beliefs[Any,EnvVar[Any]]): Beliefs[Any,EnvVar[Any]] = {
+
+  def defaultMarginalize(term: DoubleTerm, incoming: Beliefs[Any, EnvVar[Any]]): Beliefs[Any, EnvVar[Any]] = {
     val productTerm = term * Multiplication(term.variables.map(v => BeliefTerm(incoming.belief(v), v)).toSeq)
     ExhaustiveMarginalInference.infer(productTerm)
   }
