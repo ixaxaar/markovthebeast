5d6bbd4c0fe6074bb34cc6b51270e0d63759753d
sebastian.riedel <sebastian.riedel@0d9c72f9-4028-0410-bf36-39c0b5a9420c>
2010-03-23 05:37:56 +0000

    more work on StarAI paper

--- thebeast-core/src/main/scala/org/riedelcastro/thebeast/env/doubles/LogLinear.scala
+++ thebeast-core/src/main/scala/org/riedelcastro/thebeast/env/doubles/LogLinear.scala
@@ -5,12 +5,12 @@ import org.riedelcastro.thebeast.env.vectors.{Vector, VectorDotApp, VectorVar, V
 import org.riedelcastro.thebeast.solve.ExhaustiveMarginalInference
 import org.riedelcastro.thebeast.env._
 
+
 /**
  */
-case class LogLinear(sufficient:VectorTerm, weights:VectorVar, bias:DoubleTerm)
-  extends Exp(Sum(Seq(VectorDotApp(sufficient,weights),bias))) {
-
-  def marginalizeLogLinear(incoming:Beliefs[Any,EnvVar[Any]], weightsValue:Vector) : Beliefs[Any,EnvVar[Any]] = {
+case class LogLinear(sufficient: VectorTerm, weights: VectorVar, bias: DoubleTerm)
+        extends Exp(Sum(Seq(VectorDotApp(sufficient, weights), bias))) {
+  def marginalizeLogLinear(incoming: Beliefs[Any, EnvVar[Any]], weightsValue: Vector): Beliefs[Any, EnvVar[Any]] = {
     //default implementation
     val env = new MutableEnv
     //set weight variables in environment
@@ -18,9 +18,34 @@ case class LogLinear(sufficient:VectorTerm, weights:VectorVar, bias:DoubleTerm)
     //create the grounded term (that doesn't have weight variables)
     val grounded = ground(env)
     //exhaustive inference
-    ExhaustiveMarginalInference.marginalizeQueries(grounded,incoming,Set(sufficient))
+    ExhaustiveMarginalInference.marginalizeQueries(grounded, incoming, Set(sufficient))
   }
 
 }
 
-class Weights extends HashMap[VectorVar,Vector]
+/**
+ * A Featurized term is a term that deterministically depends on
+ * the value of a feature-vector * weight dot product. Very close
+ * to general linear models, but does not require normalization. Note that
+ * the weight vector must be the result of grounding a vector variable, and that
+ * that the term needs to provide this variable at request.
+ */
+trait Featurized extends DoubleTerm {
+  /**
+   * The feature vector for the given world/env.
+   */
+  def features(env: Env): Vector
+
+  /**
+   * The means/expectations of features given some beliefs for the
+   * free variable in the term, and assuming that these beliefs are independent.  
+   */
+  def means(incoming: Beliefs[Any, EnvVar[Any]]): Vector
+
+  /**
+   * The original weight vector variable that was grounded to produce the weight vector.
+   */
+  def weights: VectorVar
+}
+
+class Weights extends HashMap[VectorVar, Vector]
